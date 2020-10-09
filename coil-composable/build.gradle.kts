117e5868a4b53a4152b262edea534c1e30083d5f
Luca Spinazzola <mspinluca@gmail.com>
2020-10-09 17:55:39 -0400

    version bump

--- coil-composable/build.gradle.kts
+++ coil-composable/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         minSdkVersion(21)
         targetSdkVersion(30)
         versionCode = 1
-        versionName = "0.3.0"
+        versionName = "0.3.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
