e5121ee77348ddd491b4b226054132ffd22f532a
Luca Spinazzola <mspinluca@gmail.com>
2020-10-09 17:54:42 -0400

    update to compose 1.0.0-alpha04

--- coil-composable/build.gradle.kts
+++ coil-composable/build.gradle.kts
@@ -72,7 +72,7 @@ android {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         jvmTarget = "1.8"
-        freeCompilerArgs = listOf("-Xallow-jvm-ir-dependencies", "-Xskip-prerelease-check", "-Xjvm-default=all")
+        freeCompilerArgs = listOf("-Xallow-jvm-ir-dependencies", "-Xskip-prerelease-check")
     }
 }
 
